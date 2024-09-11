750185108b4386cd9aebc65d881cdfbc9c8c656e
Білоцерківець Сергій <sbilocerkivec@gmail.com>
2024-09-11 08:16:01 +0300

    Lesson_9.2

--- Lesson_9.2.py
+++ Lesson_9.2.py
@@ -4,6 +4,7 @@ def difference(*args):
     else:
         return 0
 
+
 assert difference(1, 2, 3) == 2, 'Test1'
 assert difference(5, -5) == 10, 'Test2'
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
