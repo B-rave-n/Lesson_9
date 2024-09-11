642c117262cbaf8686610975715a2430cb81b9ef
Білоцерківець Сергій <sbilocerkivec@gmail.com>
2024-09-11 08:15:18 +0300

    Lesson_9.1

--- Lesson_9.1.py
+++ Lesson_9.1.py
@@ -14,6 +14,7 @@ def popular_words (text, words):
     return population_of_words
 
 
+
 assert popular_words('''When I was One I, had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
 # Додаткові перевірки
 assert popular_words('''When (I) was One I, had just begun When ^I wAs Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test2'
